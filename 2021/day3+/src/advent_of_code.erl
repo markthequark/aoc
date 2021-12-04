937df66ec28cf7cfc6fc1403b999af4d096de906
Mark McElroy <mark.mcelroy@outlook.com>
2021-12-04 17:33:26 +0000

    day 3 gamma rate with read_line

---
+++ 2021/day3+/src/advent_of_code.erl
@@ -0,0 +1,25 @@
+-module(advent_of_code).
+
+%% API exports
+-export([main/1]).
+
+%%====================================================================
+%% API functions
+%%====================================================================
+
+%% escript Entry point
+-spec main([string()]) -> no_return().
+main([Day]) ->
+    Module = list_to_atom("day_" ++ Day),
+    Filename = "priv/day" ++ Day ++ ".txt",
+    Answer1 = Module:p1(Filename),
+    Answer2 = Module:p2(Filename),
+    io:format("Part 1 answer: ~p~nPart 2 answer: ~p~n", [Answer1, Answer2]),
+    erlang:halt(0);
+main(_) ->
+    io:format("Bad args, call script as ./script <day>
+    e.g. ./script 3").
+
+%%====================================================================
+%% Internal functions
+%%====================================================================
