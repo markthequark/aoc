24b2960390bc512438436b7f20184d39f10cf29a
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2021-12-05 16:14:57 +0000

    helper:read_lines/2 ignore empty lines

--- 2021/src/helper.erl
+++ 2021/src/helper.erl
@@ -3,11 +3,11 @@
 %% API
 -export([read_lines/2]).
 
-%% Returns a list of trimmed strings or binaries for each line in the file
+%% Returns a list of trimmed strings or binaries for each line in the file, ignoring empty lines
 -spec read_lines(string(), binary | string) -> [binary() | string()].
 read_lines(Filename, binary) ->
     {ok, Binary} = file:read_file(Filename),
-    binary:split(Binary, <<"\n">>, [global, trim]);
+    binary:split(Binary, <<"\n">>, [global, trim_all]);
 read_lines(Filename, string) ->
     Lines = read_lines(Filename, binary),
     lists:map(fun binary_to_list/1, Lines).
