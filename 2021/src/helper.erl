dad53dd7cd0720e56a72f5cdd726e90ca1271b9b
Mark McElroy <mark.mcelroy@outlook.com>
2021-12-05 20:16:18 +0000

    add helper function undigits

--- 2021/src/helper.erl
+++ 2021/src/helper.erl
@@ -1,7 +1,7 @@
 -module(helper).
 
 %% API
--export([read_lines/2]).
+-export([read_lines/2, undigits/1, undigits/2]).
 
 %% Returns a list of trimmed strings or binaries for each line in the file, ignoring empty lines
 -spec read_lines(string(), binary | string) -> [binary() | string()].
@@ -11,3 +11,18 @@ read_lines(Filename, binary) ->
 read_lines(Filename, string) ->
   Lines = read_lines(Filename, binary),
   lists:map(fun binary_to_list/1, Lines).
+
+%% Returns the integer represented by the ordered Digits
+%% An optional Base value may be provided representing the radix for the Digits
+undigits(Digits) ->
+  undigits(Digits, 2).
+
+undigits(Digits, Base) ->
+  undigits(Digits, Base, 0).
+
+undigits([], _Base, Acc) ->
+  Acc;
+undigits([Digit | _], Base, _) when Digit >= Base ->
+  error(badarg, io_lib:format("invalid digit ~p in base ~p~n", [Digit, Base]));
+undigits([Digit | Rest], Base, Acc) ->
+  undigits(Rest, Base, Acc * Base + Digit).
