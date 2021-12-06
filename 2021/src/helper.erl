5e19ad598d75260c24154b80e043c4fa79d856d5
Mark McElroy <mark.mcelroy@outlook.com>
2021-12-06 17:03:01 +0000

    add more general read_input/3 function to helper

--- 2021/src/helper.erl
+++ 2021/src/helper.erl
@@ -3,18 +3,24 @@
 -import(lists, [map/2]).
 
 %% API
--export([read_lines/2, undigits/1, undigits/2]).
+-export([read_input/3, read_lines/2, undigits/1, undigits/2]).
 
-%% Returns a list of trimmed strings or binaries for each line in the file, ignoring empty lines
+%% @doc Returns a list of trimmed strings or binaries for each line in the file, ignoring empty lines
 -spec read_lines(string(), binary | string) -> [binary() | string()].
-read_lines(Filename, binary) ->
+read_lines(Filename, Mode) ->
+  read_input(Filename, <<"\n">>, Mode).
+
+%% @doc Delimits the file by the given Delimiter and returns a list of trimmed strings or binaries
+-spec read_input(string(), binary() | [binary()], binary | string) ->
+                  [binary() | string()].
+read_input(Filename, Delimiter, binary) ->
   {ok, Binary} = file:read_file(Filename),
-  binary:split(Binary, <<"\n">>, [global, trim_all]);
-read_lines(Filename, string) ->
-  Lines = read_lines(Filename, binary),
+  binary:split(Binary, Delimiter, [global, trim_all]);
+read_input(Filename, Delimiter, string) ->
+  Lines = read_input(Filename, Delimiter, binary),
   map(fun binary_to_list/1, Lines).
 
-%% Returns the integer represented by the ordered Digits
+%% @doc Returns the integer represented by the ordered Digits
 %% An optional Base value may be provided representing the radix for the Digits
 undigits(Digits) ->
   undigits(Digits, 2).
