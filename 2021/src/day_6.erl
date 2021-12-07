b646d16ba28a166e34d95bd9fc30a6545f6d9080
MarkTheQuark <25646157+markthequark@users.noreply.github.com>
2021-12-07 16:40:15 +0000

    day p2

--- 2021/src/day_6.erl
+++ 2021/src/day_6.erl
@@ -1,14 +1,9 @@
 -module(day_6).
 
--import(lists, [nth/2, foldl/3, map/2, seq/2, min/1, max/1]).
-
-%% The maximum number of days a single process can simulate a single fish for
--define(SIM_DAYS, 100).
+-import(lists, [map/2]).
 
 %% API
 -export([p1/1, p2/1]).
-
--compile(export_all).
 
 %%====================================================================
 %% API functions
@@ -18,13 +13,7 @@ p1(Filename) ->
   solve(Filename, 80).
 
 p2(Filename) ->
-  %% solving for 256 uses too much memory.
-  %% perhaps we should split on list size, but we'd only know to split
-  %% after doing a bunch of work, not parallel initially
-  %%
-  %% an optimisation idea - we don't need to solve for [1,1,2,2,3,3]
-  %% we can solve for [1,2,3] and multiply the final length by 2
-  solve(Filename, 80).
+  solve(Filename, 256).
 
 %%====================================================================
 %% Internal functions
@@ -34,38 +23,20 @@ solve(Filename, Days) ->
   Input = helper:read_input(Filename, [<<",">>, <<"\n">>], string),
   Fish = map(fun list_to_integer/1, Input),
 
-  lists:foreach(fun(F) -> spawn(?MODULE, simulate, [self(), F, Days]) end, Fish),
+  NewFish = simulate(helper:frequency_map(Fish), Days),
 
-  rec_loop(length(Fish)).
+  lists:sum(
+    maps:values(NewFish)).
 
-rec_loop(N) ->
-  rec_loop(N, 0).
-
-rec_loop(0, Acc) ->
-  Acc;
-rec_loop(N, Acc) ->
-  Fish =
-    receive
-      {fish, Fish} ->
-        Fish
-    end,
-  rec_loop(N - 1, Fish + Acc).
-
-%%====================================================================
-%% Worker Processes
-%%====================================================================
-
-simulate(From, Fish, Days)
-  when is_pid(From), is_integer(Fish), is_integer(Days), Days > 0 ->
-  simulate(From, [Fish], Days);
-simulate(From, Fish, Days) when is_pid(From), is_list(Fish), is_integer(Days), Days > 0 ->
-  simulate(From, Fish, Days, []).
-
-simulate(From, Fish, 0, _) ->
-  From ! {fish, length(Fish)};
-simulate(From, [], Days, NewFish) ->
-  simulate(From, NewFish, Days - 1, []);
-simulate(From, [0 | Rest], Days, NewFish) ->
-  simulate(From, Rest, Days, [6, 8 | NewFish]);
-simulate(From, [Fish | Rest], Days, NewFish) ->
-  simulate(From, Rest, Days, [Fish - 1 | NewFish]).
+simulate(Fish, 0) ->
+  Fish;
+simulate(Fish, Days) ->
+  NewFish =
+    maps:fold(fun (0, Freq, Acc) ->
+                    maps:update_with(6, fun(V) -> Freq + V + 1 end, Freq, Acc#{8 => Freq});
+                  (Age, Freq, Acc) ->
+                    maps:update_with(Age - 1, fun(V) -> Freq + V end, Freq, Acc)
+              end,
+              #{},
+              Fish),
+  simulate(NewFish, Days - 1).
