bee5b3378d178fa51059a9db7fff729c9983cf66
Mark McElroy <mark.mcelroy@outlook.com>
2021-12-07 17:34:22 +0000

    move frequency_map function to helper module

--- 2021/src/day_5.erl
+++ 2021/src/day_5.erl
@@ -1,6 +1,6 @@
 -module(day_5).
 
--import(lists, [foldl/3, map/2, seq/2]).
+-import(lists, [map/2, seq/2]).
 
 -type point() :: {integer(), integer()}.
 -type line() :: {point(), point()}.
@@ -18,14 +18,14 @@ p1(Filename) ->
   Points = lists:flatmap(fun to_points/1, Lines),
 
   maps:size(
-    maps:filter(fun(_K, V) -> V > 1 end, histogram(Points))).
+    maps:filter(fun(_K, V) -> V > 1 end, helper:frequency_map(Points))).
 
 p2(Filename) ->
   Lines = parse_lines(helper:read_lines(Filename, string)),
   Points = lists:flatmap(fun to_points/1, Lines),
 
   maps:size(
-    maps:filter(fun(_K, V) -> V > 1 end, histogram(Points))).
+    maps:filter(fun(_K, V) -> V > 1 end, helper:frequency_map(Points))).
 
 %%====================================================================
 %% Internal functions
@@ -48,11 +48,3 @@ to_points({{X1, Y1}, {X2, Y2}}) when X2 - X1 == Y2 - Y1 ->
   map(fun(X) -> {X, Y1 + X - X1} end, seq(X1, X2));
 to_points({{X1, Y1}, {X2, Y2}}) when X2 - X1 == Y1 - Y2 ->
   map(fun(X) -> {X, Y1 - X + X1} end, seq(X1, X2)).
-
--spec inc(integer()) -> integer().
-inc(X) when is_integer(X) ->
-  X + 1.
-
--spec histogram([T]) -> #{T => integer()}.
-histogram(List) when is_list(List) ->
-  foldl(fun(Elem, Acc) -> maps:update_with(Elem, fun inc/1, 1, Acc) end, #{}, List).
