30e252e55040bc61faaacdf4f2a776087c5507b5
Mark McElroy <mark.mcelroy@outlook.com>
2021-12-09 20:58:11 +0000

    day 9 p1

--- 2021/src/day_9.erl
+++ 2021/src/day_9.erl
@@ -10,7 +10,10 @@
 %%====================================================================
 
 p1(Filename) ->
-  ok.
+  Input = helper:read_lines(Filename, string),
+  Lines = map(fun parse_line/1, Input),
+
+  sum([scan(A, B, C) || [A, B, C] <- groups_of_3(Lines)]).
 
 p2(Filename) ->
   ok.
@@ -18,3 +21,38 @@ p2(Filename) ->
 %%====================================================================
 %% Internal functions
 %%====================================================================
+
+parse_line(Line) ->
+  map(fun(Digit) -> Digit - $0 end, Line).
+
+scan(none, Mid, Bot) ->
+  scan([10 || _ <- Mid], Mid, Bot);
+scan(Top, Mid, none) ->
+  scan(Top, Mid, [10 || _ <- Mid]);
+scan([Above | _] = Top, [Elem, After | _] = Mid, [Below | _] = Bot)
+  when Elem < Above, Elem < After, Elem < Below ->
+  scan(Top, Mid, Bot, Elem + 1);
+scan([_ | _] = Top, [_ | _] = Mid, [_ | _] = Bot) ->
+  scan(Top, Mid, Bot, 0).
+
+scan([_, Above], [Before, Elem], [_, Below], Acc)
+  when Elem < Above, Elem < Before, Elem < Below ->
+  Acc + Elem + 1;
+scan([_, _], [_, _], [_, _], Acc) ->
+  Acc;
+scan([_, Above | _] = Top,
+     [Before, Elem, After | _] = Mid,
+     [_, Below | _] = Bot,
+     Acc)
+  when Elem < Above, Elem < Before, Elem < After, Elem < Below ->
+  scan(tl(Top), tl(Mid), tl(Bot), Acc + Elem + 1);
+scan(Top, Mid, Bot, Acc) ->
+  scan(tl(Top), tl(Mid), tl(Bot), Acc).
+
+groups_of_3([E1, E2 | _] = List) ->
+  groups_of_3(List, [[none, E1, E2]]).
+
+groups_of_3([E1, E2], Acc) ->
+  lists:reverse([[E1, E2, none] | Acc]);
+groups_of_3([E1 | [E2, E3 | _] = Rest], Acc) ->
+  groups_of_3(Rest, [[E1, E2, E3] | Acc]).
