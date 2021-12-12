889676f0d1b9f1f95f6bf283764b124421b7abef
Mark McElroy <mark.mcelroy@outlook.com>
2021-12-12 03:40:13 +0000

    day 9 p2 wip

--- 2021/src/day_9.erl
+++ 2021/src/day_9.erl
@@ -16,7 +16,16 @@ p1(Filename) ->
   sum([scan(A, B, C) || [A, B, C] <- groups_of_3(Lines)]).
 
 p2(Filename) ->
-  ok.
+  Input = helper:read_lines(Filename,string),
+  Grid = to_grid( Input ),
+  LowPoints =
+  [
+   Pos
+   ||
+   Pos <- maps:keys(Grid),
+   map_get(Pos,Grid) < lists:min(lists:map(fun(Pos) -> map_get(Pos,Grid) end, neighbours(Pos, Grid)))
+  ],
+  sum(lists:map(fun(Pos) -> map_get(Pos, Grid) end, LowPoints)).
 
 %%====================================================================
 %% Internal functions
@@ -40,10 +49,7 @@ scan([_, Above], [Before, Elem], [_, Below], Acc)
   Acc + Elem + 1;
 scan([_, _], [_, _], [_, _], Acc) ->
   Acc;
-scan([_, Above | _] = Top,
-     [Before, Elem, After | _] = Mid,
-     [_, Below | _] = Bot,
-     Acc)
+scan([_, Above | _] = Top, [Before, Elem, After | _] = Mid, [_, Below | _] = Bot, Acc)
   when Elem < Above, Elem < Before, Elem < After, Elem < Below ->
   scan(tl(Top), tl(Mid), tl(Bot), Acc + Elem + 1);
 scan(Top, Mid, Bot, Acc) ->
@@ -56,3 +62,16 @@ groups_of_3([E1, E2], Acc) ->
   lists:reverse([[E1, E2, none] | Acc]);
 groups_of_3([E1 | [E2, E3 | _] = Rest], Acc) ->
   groups_of_3(Rest, [[E1, E2, E3] | Acc]).
+
+neighbours({X, Y}, Grid) ->
+  Offsets = [{1, 0}, {0, -1}, {0, 1}, {-1, 0}],
+  [Pos || {XOff, YOff} <- Offsets, Pos <- [{X + XOff, Y + YOff}], is_map_key(Pos, Grid)].
+
+to_grid(ListOfLists) ->
+  maps:from_list([{{X, Y}, Val - $0}
+                  || {X, Row}
+                       <- lists:zip(
+                            lists:seq(1, length(ListOfLists)), ListOfLists),
+                     {Y, Val}
+                       <- lists:zip(
+                            lists:seq(1, length(Row)), Row)]).
